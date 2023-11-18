40bcf62e778ab7b7e77ddd56f1fc286889652564
Yury Stepanchuk <stich-style@mail.ru>
2023-11-18 16:01:42 +0300

    Initial Commit

---
+++ SCAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SCAnimation
+//
+//  Created by Юрий Степанчук on 18.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
